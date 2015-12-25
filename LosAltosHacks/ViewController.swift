abbcdc5e256d4c9ab853dfcd4233eff8b4973c75
Dan Appel <dan.appel00@gmail.com>
2015-12-25 21:58:37 +0000

    Initial Commit

---
+++ LosAltosHacks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LosAltosHacks
+//
+//  Created by Dan Appel on 12/25/15.
+//  Copyright © 2015 Dan Appel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
