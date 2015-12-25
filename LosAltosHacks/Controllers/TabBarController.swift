30124e9ae83e4472d5635bab1c4cb26c5f29bc87
Dan Appel <dan.appel00@gmail.com>
2015-12-25 23:26:02 +0000

    Make initial tab the middle one

---
+++ LosAltosHacks/Controllers/TabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  TabBarController.swift
+//  LosAltosHacks
+//
+//  Created by Dan Appel on 12/25/15.
+//  Copyright © 2015 Dan Appel. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    override func viewDidLoad() {
+        self.selectedIndex = 2
+    }
+}
