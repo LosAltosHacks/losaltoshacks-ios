febf8948b1ccceb53029855d84b4e85d50af8bf2
Dan Appel <dan.appel00@gmail.com>
2015-12-28 01:50:30 +0000

    Migrate to SnapKit for FAQ screen

--- LosAltosHacks/Controllers/FAQViewController.swift
+++ LosAltosHacks/Controllers/FAQViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SnapKit
 
 struct FAQ {
     var question: String
@@ -19,12 +20,16 @@ class FAQViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var slackButton: UIButton!
+    @IBOutlet weak var helpLabel: UILabel!
 
+    @IBOutlet weak var mentorView: UIView!
     // MARK: Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupConstraints()
+        
         // View Styles
 
         slackButton.layer.cornerRadius = 20
@@ -39,6 +44,45 @@ class FAQViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
 
+    }
+    
+    func setupConstraints() {
+        
+        // Mentor View
+        mentorView.snp_makeConstraints { make in
+            make.height.equalTo(128)
+            make.centerX.equalTo(view.snp_centerX)
+
+            make.leading.equalTo(view.snp_leading)
+            make.top.equalTo(snp_topLayoutGuideBottom)
+        }
+        
+        helpLabel.snp_makeConstraints { make in
+            make.width.equalTo(267)
+            make.height.equalTo(21)
+            
+            make.centerX.equalTo(mentorView.snp_centerX)
+            make.top.equalTo(mentorView.snp_top).offset(24)
+        }
+        
+        slackButton.snp_makeConstraints { make in
+            make.width.equalTo(279)
+            make.height.equalTo(44)
+            
+            make.centerX.equalTo(helpLabel.snp_centerX)
+            make.top.equalTo(helpLabel.snp_bottom).offset(16)
+        }
+        
+        
+        // Table view
+        tableView.snp_makeConstraints { make in
+            make.top.equalTo(mentorView.snp_bottom)
+            
+            make.width.equalTo(view.snp_width)
+            
+            // uhh
+            make.bottomMargin.equalTo(view.snp_bottomMargin).offset(-57)
+        }
     }
 
     override func didReceiveMemoryWarning() {
