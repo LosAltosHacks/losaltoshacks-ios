110e76564c364a27356987ec3d1fa394da67f68a
Dan Appel <dan.appel00@gmail.com>
2016-01-03 12:50:25 +0300

    Make fetching of updates safer and more likely to be successful

--- LosAltosHacks/Models/Update.swift
+++ LosAltosHacks/Models/Update.swift
@@ -13,23 +13,41 @@ struct Update {
     let date: NSDate
     let description: String
     
-    static func getUpdates(callback: ([Update]) -> Void) /*-> [Update]*/ {
+    /**
+     Makes an asynchronous request to the LAH API to fetch the latest updates,
+     and calls the callback on success.
+     Recursively calls itself up to 3 times until is successful.
+     */
+    static func getUpdates(callback: [Update] -> Void) {
+        getUpdates(recursiveDepth: 0, callback: callback)
+    }
+    
+    /**
+     Internal function which does the actual recursion.
+     */
+    private static func getUpdates(recursiveDepth recursiveDepth: Int, callback: [Update] -> Void) {
         
-        // make a network request here
-
+        guard recursiveDepth < 3 else { return }
+        
         let url = LAHConstants.BaseAPIURLString + "updates.json"
-        Alamofire.request(.GET, url)
-                 .responseJSON { response in
-                    print(response)
-                    let rawUpdates = try! NSJSONSerialization.JSONObjectWithData(response.data!, options: .AllowFragments)
-                    let updates = (rawUpdates as! [[String:AnyObject]])
-                        .map {
-                            Update(
-                                date: NSDate(timeIntervalSince1970: NSTimeInterval($0["date"]!.intValue)),
-                                description: $0["description"] as! String
-                            )
-                    }
-                    callback(updates)
-                 }
+        
+        Alamofire.request(.GET, url).responseJSON { response in
+            print(response)
+            
+            guard let rawUpdates = try? NSJSONSerialization.JSONObjectWithData(response.data!, options: .AllowFragments) else {
+                // recurse
+                getUpdates(recursiveDepth: recursiveDepth+1, callback: callback)
+                return
+            }
+            
+            let updates = (rawUpdates as! [[String:AnyObject]])
+                .map {
+                    Update(
+                        date: NSDate(timeIntervalSince1970: NSTimeInterval($0["date"]!.intValue)),
+                        description: $0["description"] as! String
+                    )
+            }
+            callback(updates)
+        }
     }
 }
