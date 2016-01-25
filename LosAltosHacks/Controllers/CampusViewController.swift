02f3c7b758495bd0c516d8404488956c4a6ef3b0
Justin Yu <justin.v.yu@gmail.com>
2016-01-24 23:11:08 -0800

    Add different resolutions for images and fix campus map scrolling

--- LosAltosHacks/Controllers/CampusViewController.swift
+++ LosAltosHacks/Controllers/CampusViewController.swift
@@ -35,7 +35,11 @@ class CampusViewController: BaseViewController {
         scrollView.delegate = self
         scrollView.minimumZoomScale = 1.0
         scrollView.maximumZoomScale = 5.0
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.clipsToBounds = false
 
+        segmentedControl.layer.zPosition = 1.0
         segmentedControl.layer.cornerRadius = 5.0
 
         imageView.contentMode = .ScaleAspectFit
@@ -45,45 +49,11 @@ class CampusViewController: BaseViewController {
     }
 
     override func setupConstraints() {
-
-        scrollView.snp_makeConstraints { make in
-            make.left.equalTo(view.snp_left).offset(15)
-            make.right.equalTo(view.snp_right).offset(-15)
-//            make.center.equalTo(view.snp_center)
-            make.centerY.equalTo(view.snp_centerY)
-            make.bottom.equalTo(snp_bottomLayoutGuideTop).offset(-10)
-//            make.top.equalTo(segmentedControl.snp_bottom).offset(10)
-        }
-
         segmentedControl.snp_makeConstraints { make in
             make.top.equalTo(snp_topLayoutGuideBottom).offset(10)
             make.centerX.equalTo(view.snp_centerX)
         }
-
-        imageView.snp_makeConstraints { make in
-            make.top.equalTo(scrollView.snp_top)
-            make.left.equalTo(scrollView.snp_left)
-            make.bottom.equalTo(scrollView.snp_bottom)
-            make.right.equalTo(scrollView.snp_right)
-        }
     }
-
-//    func setupMapViews() {
-//        let maps = [UIImage(named: "floor2"), UIImage(named: "floor2")]
-//
-////        scrollView.pagingEnabled = true
-//
-//        let width = self.view.frame.size.width - 2 * 10
-//
-//        scrollView.contentSize = CGSizeMake(CGFloat(maps.count) * width,
-//                                            width)
-////        let mapViews = maps.map { GestureImageView(image: $0) }
-//        maps.enumerate().forEach { index, mapImage in
-//            let view = UIImageView(image: mapImage!)
-//            view.frame = CGRectMake(CGFloat(index) * width + 10, 0, width - 20, width)
-//            scrollView.addSubview(view)
-//        }
-//    }
 
 }
 
