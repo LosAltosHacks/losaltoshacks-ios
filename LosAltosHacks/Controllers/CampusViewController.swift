5c67a4a253b8c245f272d7858f52f2c3b6da8029
Dan Appel <dan.appel00@gmail.com>
2017-01-24 23:20:41 -0800

    Update campus map

--- LosAltosHacks/Controllers/CampusViewController.swift
+++ LosAltosHacks/Controllers/CampusViewController.swift
@@ -11,23 +11,7 @@ import UIKit
 class CampusViewController: BaseViewController {
 
     @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var imageView: UIImageView!
-
-    // TODO: Add gestures for zoom/pan
-
-    @IBAction func changeFloor(_ sender: UISegmentedControl) {
-        switch(sender.selectedSegmentIndex) {
-        case 0:
-            imageView.image = UIImage(named: "floor1")
-        case 1:
-            imageView.image = UIImage(named: "floor2")
-        default:
-            imageView.image = nil
-        }
-
-        scrollView.zoomScale = 1.0
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,22 +23,12 @@ class CampusViewController: BaseViewController {
         scrollView.showsVerticalScrollIndicator = false
         scrollView.clipsToBounds = false
 
-        segmentedControl.layer.zPosition = 1.0
-        segmentedControl.layer.cornerRadius = 5.0
-
         imageView.contentMode = .scaleAspectFit
         imageView.isUserInteractionEnabled = true
-
-//        setupMapViews()
     }
 
     override func setupConstraints() {
-        segmentedControl.snp.makeConstraints { make in
-            make.top.equalTo(topLayoutGuide.snp.bottom).offset(10)
-            make.centerX.equalTo(view.snp.centerX)
-        }
     }
-
 }
 
 extension CampusViewController: UIScrollViewDelegate {
